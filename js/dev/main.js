49bbdc20272e9ba56530d2f8a7a7169e4531268a
Feifei HANG <l.daniel.hung@gmail.com>
2014-01-17 23:34:56 +0000

    Tried to add loading hint information. However, structure and CSS have to be further changed. Consider to finish this part next week.

--- js/dev/main.js
+++ js/dev/main.js
@@ -57,6 +57,7 @@ require(['resultView'], function (resultView) {
         },
         'search': function () {
             var html = '<div id="loading-spinner"><i class="fa fa-spinner fa-spin fa-5x"></i> Loading magic...</div>';
+            // html += '<div id="hints">Why so slow?<br/>Well, congratulations! That means we have found tons of candidate services for you. Since we insist to present you all the services as a whole rather than separating them into pages, it may takes a bit while to load the data from our server.<br/>Why not take a break while we are preparing your search results for you?&nbsp;<i class="fa fa-smile-o"></i></div>';
             view.render();
             $('#home-container').css('display', 'none');
             $('#result-container').css('display', 'initial');
